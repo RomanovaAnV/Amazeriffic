0c2b7b1b59c5203308dd707443c162d6f3306dc6
RomanovaAnV <aro2801@mail.ru>
2021-05-31 17:15:29 +0300

    5.2 new app

--- 5/app.js
+++ 5/app.js
@@ -1,17 +1,14 @@
-"use strict";
+var main = function (toDoObjects) {
+	"use strict";
+	// как main имеет доступ к списку задач!
+	var toDos = toDoObjects.map(function (toDo) {
+		// просто возвращаем описание этой задачи
+		return toDo.description;
+	});
+	// сейчас весь старый код должен работать как раньше!
+	$("document").ready( function(){
 
-var toDos = [
-"Закончить писать эту книгу",
-"Вывести Грейси на прогулку в парк", 
-"Ответить на электронные письма", 
-"Подготовиться к лекции в понедельник", 
-"Обновить несколько новых задач", 
-"Купить продукты"
-];
-
-$("document").ready( function(){
-
-$(".tabs a span").toArray().forEach(function (element) { 
+	$(".tabs a span").toArray().forEach(function (element) { 
 	// создаем обработчик щелчков для этого элемента 
 	$(element).on("click", function () {
 		// поскольку мы используем версию элемента jQuery,
@@ -49,10 +46,16 @@ $(".tabs a span").toArray().forEach(function (element) {
 		
 		}
 		return false;
+		})
 	})
-})
+	$(".tabs a:first-child span").trigger("click");
 
+	})
+};	
 
-$(".tabs a:first-child span").trigger("click");
-
-})
+$(document).ready(function () {
+	$.getJSON("todos.json", function (toDoObjects) {
+	// вызов функции main с аргументом в виде объекта toDoObjects 
+		main(toDoObjects);
+	});
+});
