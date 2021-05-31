a3d32b7becd6c0a23a914d1acbd05ec37bb31c8d
RomanovaAnV <aro2801@mail.ru>
2021-05-31 17:33:15 +0300

    5.3 add vkladka tag

--- 5/app.js
+++ 5/app.js
@@ -9,28 +9,47 @@ var main = function (toDoObjects) {
 	$("document").ready( function(){
 
 	$(".tabs a span").toArray().forEach(function (element) { 
-	// создаем обработчик щелчков для этого элемента 
-	$(element).on("click", function () {
-		// поскольку мы используем версию элемента jQuery,
-		// нужно создать временную переменную,
-		// чтобы избежать постоянного обновления
-		var $element = $(element);
-		$(".tabs a span").removeClass("active"); 
-		$(element).addClass("active");
-		$("main .content").empty();
+		// создаем обработчик щелчков для этого элемента 
+		$(element).on("click", function () {
+			// поскольку мы используем версию элемента jQuery,
+			// нужно создать временную переменную,
+			// чтобы избежать постоянного обновления
+			var $element = $(element);
+			$(".tabs a span").removeClass("active"); 
+			$(element).addClass("active");
+			$("main .content").empty();
+			$("main .photos").empty();
+			if ($element.parent().is(":nth-child(1)")) { 
+				for (var i = toDos.length-1; i > -1; i--) { 
+					$(".content").append($("<li>").text(toDos[i]));
+				}
+			} 
+			else if ($element.parent().is(":nth-child(2)")) { 
+				toDos.forEach(function (todo) { 
+					$(".content").append($("<li>").text(todo));
+				});
+			} 
+			else if ($element.parent().is(":nth-child(3)")) { 
+				// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ 
+				console.log("Щелчок на вкладке Теги");
+				// organizeByTag(toDoObjects);
+				var organizedByTag = organizeByTags(toDoObjects);
+				
+				 organizedByTag.forEach(function (tag) { 
+					var $tagName = $("<h3>").text(tag.name), 
+					$content = $("<ul>"); 
+					tag.toDos.forEach(function (description) { 
+						var $li = $("<li>").text(description); 
+						$content.append($li);
+					});
+					$("main .content").append($tagName); 
+					$("main .content").append($content);
+				});
 
-		if ($element.parent().is(":nth-child(1)")) { 
-			for (var i = toDos.length-1; i > -1; i--) { 
-				$(".content").append($("<li>").text(toDos[i]));
-			}
-		} 
-		else if ($element.parent().is(":nth-child(2)")) { 
-			toDos.forEach(function (todo) { 
-				$(".content").append($("<li>").text(todo));
-			});
-		} 
-		else if ($element.parent().is(":nth-child(3)")) { 
-			$(".content").append(
+
+			} 
+			else if ($element.parent().is(":nth-child(4)")) { 
+				$(".content").append(
 				'<input type="text" class="inp">'+
 				'<button class="btn">Добавить</button>'
 			);
@@ -42,10 +61,11 @@ var main = function (toDoObjects) {
 					alert('Новое задание "'+newToDo+'" успешно добавлено!');
 					$('.inp').val("");
 				}
-			})
-		
-		}
-		return false;
+				});
+				
+			}
+			
+			return false;
 		})
 	})
 	$(".tabs a:first-child span").trigger("click");
