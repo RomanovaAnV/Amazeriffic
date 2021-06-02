31e211f10d2a55322246fc452bd9314a05e930c2
RomanovaAnV <aro2801@mail.ru>
2021-06-02 14:18:51 +0300

    rabota func

--- 4/app.js
+++ 4/app.js
@@ -1,20 +1,19 @@
 var main = function () 
 {"use strict";
-$(".tabs a:nth-child(1)").on("click", function () {
-$(".tabs span").removeClass("active");
-$(".tabs a:nth-child(1) span").addClass("active");
-$("main .content").empty();
+var makeTabActive = function (tabNumber) {
+	var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+	$(".tabs span").removeClass("active");
+	$(tabSelector).addClass("active");};
+	$(".tabs a:nth-child(1)").on("click", function () {
+	makeTabActive(1);
+	return false;
 });
 $(".tabs a:nth-child(2)").on("click", function () {
-$(".tabs span").removeClass("active");
-$(".tabs a:nth-child(2) span").addClass("active");
-$("main .content").empty();
-return false;
+	makeTabActive(2);
+	return false;
 });
 $(".tabs a:nth-child(3)").on("click", function () {
-$(".tabs span").removeClass("active");
-$(".tabs a:nth-child(3) span").addClass("active");
-$("main .content").empty();
-return false;
+	makeTabActive(3);
+	return false;
 });
 };
