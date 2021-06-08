25042bb3a9a7370a73f693ac9802f199252e014c
RomanovaAnV <aro2801@mail.ru>
2021-06-08 19:52:27 +0300

    8.2 add users controller

---
+++ 8/controllers/users_controller.js
@@ -0,0 +1,28 @@
+var User = require("../models/user.js"),
+mongoose = require("mongoose");
+var UsersController = {};
+UsersController.index = function (req, res) {    
+    console.log("вызвано действие: индекс");
+    res.send(200);
+};
+// Отобразить пользователя
+UsersController.show = function (req, res) {    
+    console.log("вызвано действие: показать");
+    res.send(200);
+};
+    // Создать нового пользователя
+    UsersController.create = function (req, res) {
+        console.log("вызвано действие: создать");
+        res.send(200);
+    };
+    // Обновить существующего пользователя
+    UsersController.update = function (req, res) {
+        console.log("вызвано действие: обновить");
+        res.send(200);
+    };
+    // Удалить существующего пользователя
+    UsersController.destroy = function (req, res) {
+        console.log("destroy action called");
+        res.send(200);
+    };
+    module.exports = UsersController;
